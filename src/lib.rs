b04273dbbb51a9534e3e871fde3e8af02afd3b00
Devin Sterling <devinsterling@outlook.com>
2022-12-29 21:31:03 -0500

    Updated files

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub fn empty() {}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn test() {}
 }
